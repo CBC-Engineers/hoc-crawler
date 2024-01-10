4b2a04cd93e132fe76ec15d1761607e9c97ec7cb
Rick Teachey <ricky@teachey.org>
2024-01-10 16:08:57 -0500

    Version 0.1

--- hoc_crawler/__init__.py
+++ hoc_crawler/__init__.py
@@ -1,6 +1,6 @@
 """Figure out the height of cover limits for buried pipes."""
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 from .hoc_crawler import crawl, TargetHOC, InvalidHOC
 from .hoc_range import hoc_range
